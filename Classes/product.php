88b0630f03ecb239088c0c3dd087cc0a9f9edd8d
Alessandro Oviszach <78302740+AlessandroOviszach@users.noreply.github.com>
2022-01-10 16:03:42 +0100

    setup files

---
+++ Classes/product.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+$porduct_selection = [
+    'clothes' => [
+        [
+            'name' => 'T-shirt',
+            'price' => '30',
+            'in stock' => true
+        ],
+        [
+            'name' => 'sweatshirt',
+            'price' => '60',
+            'in stock' => false
+        ],
+        [
+            'name' => 'socks',
+            'price' => '10',
+            'in stock' => true
+        ],
+        
+    ]
+]
+
+
+
+
+
+?>
